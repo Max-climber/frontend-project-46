899ce77af6bf7ce7fabeac44d83f5b472ae414f8
Max-climber <max.izrailev04@mail.ru>
2025-02-19 00:37:41 +0300

    update description for .helpOption

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
 .name('gendiff')
 .description('compare files and shows a difference')
 .version('1.0.0', '-V, --version', 'output the version number') 
-.helpOption('-h, --help', 'show info')
+.helpOption('-h, --help', 'output usage information')
 .parse(process.argv);
 
 gendiff();
