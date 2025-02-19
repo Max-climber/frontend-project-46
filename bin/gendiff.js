b0411dbde12890956b617814394bd955a8d390f1
Max-climber <max.izrailev04@mail.ru>
2025-02-19 15:21:11 +0300

    add option --format and action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
 import gendiff from '../src/gendiff.js';
-import { program } from 'commander';
+import { Command } from 'commander';
+
+const program = new Command();
 
 program
-.name('gendiff')
-.description('compare files and shows a difference')
-.version('1.0.0', '-V, --version', 'output the version number') 
-.helpOption('-h, --help', 'output usage information')
-.parse(process.argv);
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0', '-V, --version', 'output the version number') 
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>', 'path to the first file')
+    .argument('<filepath2>', 'path to the second file')
+    .helpOption('-h, --help', 'output usage information')
+    .action((filepath1, filepath2, options) => {
+        const format = options.format;
+        const diff = gendiff(filepath1, filepath2, format);
+        console.log(diff);
+    })
+    
+    program.parse(process.argv);
 
-gendiff();
+gendiff()
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,9 @@
 // src/gendiff.js
-const gendiff = () => {
-    console.log("Gendiff function called!"); // Временно, для проверки
+const gendiff = (filepath1, filepath2, format) => {
+    console.log('filepath1:', filepath1); // Временно, для проверки
+    console.log('filepath2:', filepath2); // Временно, для проверки
+    console.log('format:', format); // Временно, для проверки
+    return 'Diff result'; // Временно, для проверки
   };
   
   export default gendiff;
