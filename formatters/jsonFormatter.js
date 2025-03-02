564ec91fb9adcaebac72776aa03b563d07099365
Max-climber <max.izrailev04@mail.ru>
2025-03-02 19:32:29 +0300

    update jsonFormatter

--- formatters/jsonFormatter.js
+++ formatters/jsonFormatter.js
@@ -1,32 +1,33 @@
 const buildJson = (AST) => {
-    const result = {};
-  
-    AST.forEach((node) => {
-      const { key, type, value, oldValue, newValue, children } = node;
-  
-      switch (type) {
-        case 'nested':
-          result[key] = buildJson(children);
-          break;
-        case 'added':
-          result[key] = value;
-          break;
-        case 'deleted':
-          result[key] = null;
-          break;
-        case 'changed':
-          result[key] = newValue;
-          break;
-        case 'unchanged':
-          result[key] = value; 
-          break;
-        default:
-          throw new Error(`Unknown type: ${type}`);
-      }
-    });
-  
-    return result;
-  };
-  
-  export default (AST) => JSON.stringify(buildJson(AST), null, 2);
+  const result = {};
 
+  AST.forEach((node) => {
+    const {
+      key, type, value, newValue, children,
+    } = node;
+
+    switch (type) {
+      case 'nested':
+        result[key] = buildJson(children);
+        break;
+      case 'added':
+        result[key] = value;
+        break;
+      case 'deleted':
+        result[key] = null;
+        break;
+      case 'changed':
+        result[key] = newValue;
+        break;
+      case 'unchanged':
+        result[key] = value;
+        break;
+      default:
+        throw new Error(`Unknown type: ${type}`);
+    }
+  });
+
+  return result;
+};
+
+export default (AST) => JSON.stringify(buildJson(AST), null, 2);
