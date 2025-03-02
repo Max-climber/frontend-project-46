c0ddfdcc8001b9d216ed32a698f71cf067d1f353
Max-climber <max.izrailev04@mail.ru>
2025-03-02 11:17:41 +0300

    update package.json

--- src/index.js
+++ src/index.js
@@ -15,38 +15,39 @@ export default (filepath1, filepath2, format = 'stylish') => {
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
     const keys = _.sortBy(_.union(keys1, keys2));
-
+  
     return keys.map((key) => {
       if (!Object.hasOwn(obj2, key)) {
         return { type: 'deleted', key, value: obj1[key] };
       }
-
+  
       if (!Object.hasOwn(obj1, key)) {
         return { type: 'added', key, value: obj2[key] };
       }
-
+  
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
         return { type: 'nested', key, children: buildAST(obj1[key], obj2[key]) };
       }
-
+  
       if (obj1[key] === obj2[key]) {
         return { type: 'unchanged', key, value: obj1[key] };
       }
-
       return {
         type: 'changed', key, oldValue: obj1[key], newValue: obj2[key],
       };
     });
   };
+  
 
   const genDiff = (obj1, obj2) => {
     const AST = buildAST(obj1, obj2);
     const formatter = getFormatter(format);
+    console.log('Generated AST:', JSON.stringify(AST, null, 2));
     if (!formatter) {
       throw new Error(`Unknown format: ${format}`);
     }
     return `${formatter(AST)}`;
   };
-
+  
   return genDiff(parsedData1, parsedData2);
 };
